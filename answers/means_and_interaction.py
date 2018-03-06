da99ad0e396e48299f08443d95d587c962e2131a
thach <thach4567@gmail.com>
2018-03-06 23:59:45 +0700

    first commit

---
+++ answers/means_and_interaction.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import sys
+if sys.version >= '3':
+    long = int
+from pyspark.sql import SparkSession
+
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("thach")\
+        .getOrCreate()
+    lines = spark.read.text("./sample_movielens_ratings.txt").rdd
+    parts = lines.map(lambda row: row.value.split("::"))
+    ratingsRDD = parts.map(lambda p: Row(Iduser=int(p[0]), movieId=int(p[1]),
+                                         rating=float(p[2]), timestamp=long(p[3])))
+    ratings = spark.createDataFrame(ratingsRDD)
+    (training, test) = ratings.randomSplit([0.8, 0.2], seed=sys.argv[1])   
+    
+    for elem in training.limit(17).collect():
+        print(elem)
+    spark.stop()
